7e58c1bae94382788bab50448c3e528f4eab87a8
Your Name01~ <you@example.com>
2023-06-20 02:59:31 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Offers from "./Pages/Offers";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import ForgotPassword from "./Pages/ForgotPassword";
 function App() {
   return (
-    <div>
-      <h1 className='text-2xl bg-red-400'>
-        Hello, World!
-      </h1>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
